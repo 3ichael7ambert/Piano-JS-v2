48470e911cd8e0c6d691eb7705ea28099e0e6aa7
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-03-23 20:07:46 -0600

    commit2

--- piano.js
+++ piano.js
@@ -15,3 +15,31 @@ keys.forEach(key => {
     audio.play();
   });
 });
+
+
+
+///KEYS
+document.addEventListener('keydown', (e) => {
+  const kc = String.fromCharCode(e.keyCode);
+
+  const key = document.querySelector(`div[data-key='${kc}']`);
+  if (!key) return;
+
+  key.classList.add("active");
+
+  const allowed_keys = [
+      "A", "W", "S", "D",
+      "R", "F", "G", "T",
+      "H", "J", "K", "E"
+  ];
+  if (allowed_keys.includes(kc)) play(allowed_keys.indexOf(kc) + 1);
+});
+
+document.addEventListener('keyup', (e) => {
+  const kc = String.fromCharCode(e.keyCode);
+  const box = document.querySelector(`div[data-key='${kc}']`);
+  if (!box) return;
+
+  key.classList.remove("active");
+  key.classList.remove("active");
+});
